79761296d992dbeb7ec1e2e03819dfd961d74b95
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-20 16:01:12 -0700

    add state get and set founction in local storage, add is-dead function, add update view function

--- utils/local-storage.js
+++ utils/local-storage.js
@@ -1,17 +1,31 @@
+const STATE = 'STATE';
+
+
 export function getFromLocalStorage(key) {
 
     const item = localStorage.getItem(key);
     return JSON.parse(item);
-   
+
 }
-   
-   
-   
+
 export function setInLocalStorage(key, value) {
-   
+
     const stringyItems = JSON.stringify(value);
     localStorage.setItem(key, stringyItems);
-   
+
     return value;
-   
+
 }
+
+
+export function saveState(state) {
+    const stringyState = JSON.stringify(state);
+
+    localStorage.setItem(STATE, stringyState);
+}
+
+export function getstate() {
+    const stringyState = localStorage.getItem(STATE);
+
+    return JSON.parse(stringyState);
+} 
