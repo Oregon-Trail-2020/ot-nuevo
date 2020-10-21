f8affb2c39787ad8d8a73699b55607ecd9299e8d
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-21 15:30:33 -0700

    add soceLocation function

--- location/location.js
+++ location/location.js
@@ -1,6 +1,22 @@
 import { updateView } from '../utils/update-view.js';
 
-
 updateView();
 
 // Add complete === true
+export function scoreLocation(choice, landmarkId, user) {
+    // add baddog to dog according to the choice's consequences
+    user.energy += choice.energy;
+    // add treats to the user according to the choices consqencuewbnf
+    user.food -= choice.food;
+    // set this adventure id to completed in the user
+    user.completed[landmarkId] = true;
+}
+
+
+
+
+const button = document.getElementById('space-bar');
+
+button.addEventListener('click', () => {
+    window.location.href = '../choices/choices.html';
+});
