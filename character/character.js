ae67a863b738489ca97656783266ce978dfda986
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-20 19:04:05 -0700

    added character-data and character.js

---
+++ character/character.js
@@ -0,0 +1,42 @@
+import { setInLocalStorage } from '../utils/local-storage.js';
+
+const button = document.getElementById('enter-key');
+const input = document.querySelector('input');
+
+button.addEventListener('click', () => {
+    const characterPick = Number(input.value);
+
+    if (characterPick === 1) {
+            const user = {
+                name: data.get('name'),
+                character: data.get('character'),
+                complete:
+            };
+            setInLocalStorage(user);
+        }
+    if (characterPick === 2) {
+            const user = {
+                name: data.get('name'),
+                character: data.get('character'),
+                complete:
+            };
+            setInLocalStorage(user);
+        }
+    if (characterPick === 3) {
+            const user = {
+                name: data.get('name'),
+                character: data.get('character'),
+                complete:
+            };
+            setInLocalStorage(user);
+        }
+    if (characterPick === 4) {
+            const user = {
+                name: data.get('name'),
+                character: data.get('character'),
+                complete:
+            };
+            setInLocalStorage(user);
+        }
+    window.location.href = '../store/supply-store.html';
+});
---
+++ data/character-data.js
@@ -0,0 +1,23 @@
+const character = [
+    {
+    id: character1,
+    description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    energy: 100,
+    money: 1000,
+    },{
+    id: character2,
+    description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    energy: 100,
+    money: 1500,
+    },{
+    id: character3,
+    description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    energy: 100,
+    money: 700,
+    },{
+    id: character4,
+    description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    energy: 100,
+    money: 500,
+    }
+];
