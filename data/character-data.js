76dca20cb4ef51dc111adc2b48af312648036066
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-22 17:52:42 -0700

    character-data added

--- data/character-data.js
+++ data/character-data.js
@@ -1,27 +1,27 @@
 export const character = [
     {
         id: 'character1',
-        name: 'someName',
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        name: 'Beg-packer',
+        description: 'In the age of social media, crowdfunding, and hashtags, the dream of free travel has morphed once again, giving us the phenomenon of #begpackers: People who travel backpacker-style on a beggar’s budget, asking for contributions, freebies, and handouts from locals or fellow travelers to as they go',
         energy: 100,
-        money: 1000,
+        money: 500,
     }, {
         id: 'character2',
-        name: 'someName',
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        name: 'Trust Fund Baby',
+        description: 'A young person whose parents are wealthy and have set up a trust for their son or daughter. The trust fund ensures that the child will be taken care of financially for life.',
         energy: 100,
         money: 1500,
     }, {
         id: 'character3',
-        name: 'someName',
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        name: 'Nature Enthusiast',
+        description: ' A person who enjoys being outdoors. One who holds the view that environment, rather than heredity or culture, is the primary factor in the development of an individual or group. One who loves woods or forests.',
         energy: 100,
-        money: 700,
+        money: 1000,
     }, {
         id: 'character4',
-        name: 'someName',
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        name: 'Mixologist',
+        description: '“A mixologist is an individual with a passion for combining elixirs and creating extraordinary cocktails, whereas a bartender is an individual with a passion for making great drinks and creating well-balanced experiences. To be successful, you really need both types of pros behind the bar.”',
         energy: 100,
-        money: 500,
+        money: 700,
     }
 ];
