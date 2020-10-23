fdb77ddc58086b8033f7206089485121c2779d97
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-23 14:57:27 -0700

    pathing on bio images

--- utils/update-view.js
+++ utils/update-view.js
@@ -4,27 +4,14 @@ const user = 'USER';
 
 function updateView() {
      /* reference needed DOM elements */
-    // const weather = document.getElementById('weather');
     const energy = document.getElementById('energy');
     const food = document.getElementById('food');
-    // console.log(weather, energy, food);
 
     /* initialize: load and parse json with state from local storage */
     const updateState = getState(user);
 
-    // // copy data from object to DOM properties:
-    // weather.textContent = dom.weather;
-    
-    // if they're dead, say so in the header
-    // if (isDead(updateState)) {
-    //     energy.textContent === 'You died.';
-    //     food.textContent = 'You ran out of food and died.';
-    // }
-    // else {
-        // otherwise show the bad dog status\
     energy.textContent = updateState.energy;
     food.textContent = updateState.food;
-// }
 }
 
 export default updateView;
