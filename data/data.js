897a73b8353dc6294cc8d83e9be577a04ccb1d4c
sarah-svg <kjskillen@gmail.com>
2020-10-22 18:19:47 -0700

    Landmark description

--- data/data.js
+++ data/data.js
@@ -8,7 +8,7 @@ export const landmark = [
             top: '96%',
             left: '24%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'San Diego is a city in the U.S. state of California on the coast of the Pacific Ocean, approximately 120 miles (190 km) south of Los Angeles and immediately adjacent to the border with Mexico.',
         choices: [
             {
                 id: 'map',
@@ -49,7 +49,7 @@ export const landmark = [
             top: '88%',
             left: '20%'
         },
-        description: 'I am in LA baby! dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Los Angeles officially the City of Los Angeles and often known by its initials L.A., is the largest city in California. With an estimated population of nearly four million people,[17] it is the second-most populous city in the United States (after New York City) and the third-most populous city in North America (after Mexico City and New York City). Los Angeles is known for its Mediterranean climate, ethnic diversity, Hollywood entertainment industry, and its sprawling metropolis.',
         choices: [
             {
                 id: 'map',
@@ -87,7 +87,7 @@ export const landmark = [
             top: '80%',
             left: '14%'
         },
-        description: 'The Grapevive. Hi there dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Grapevine is an unincorporated community in Kern County, California, at the southern end of the San Joaquin Valley. The small village is directly adjacent to Interstate 5 and consists mainly of travelers and roadside services. At an elevation of 1,499 feet (457 m), the community is located at the foot of a grade known as The Grapevine that starts at the mouth of Grapevine Canyon, immediately south of the community, and ascends the canyon to the Tejon Pass, which separates the Tehachapi Mountains from the San Emigdio Mountains via Interstate 5 (formerly U.S. Route 99).',
         choices: [
             {
                 id: 'map',
@@ -125,7 +125,7 @@ export const landmark = [
             top: '67%',
             left: '6%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Big Sur is a rugged stretch of California’s central coast between Carmel and San Simeon. Bordered to the east by the Santa Lucia Mountains and the west by the Pacific Ocean, it’s traversed by narrow, 2-lane State Route 1, known for winding turns, seaside cliffs and views of the often-misty coastline.',
         choices: [
             {
                 id: 'map',
@@ -163,7 +163,7 @@ export const landmark = [
             top: '59%',
             left: '5%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'San Francisco, officially the City and County of San Francisco, is the cultural, commercial, and financial center of Northern California. San Francisco is the 16th most populous city in the United States, and the fourth most populous in California.',
         choices: [
             {
                 id: 'map',
@@ -201,7 +201,7 @@ export const landmark = [
             top: '55%',
             left: '9%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Napa County is north of San Francisco, in California. Its known for hundreds of hillside vineyards in the Napa Valley wine region. In the city of Napa, Oxbow Public Market features regional gourmet food. The Napa Valley Wine Train is a vintage locomotive and traveling restaurant running through the valley.',
         choices: [
             {
                 id: 'map',
@@ -239,7 +239,7 @@ export const landmark = [
             top: '39%',
             left: '11%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Shasta is a census-designated place in Shasta County, California, United States. Shasta sits at an elevation of 843 feet. The 2010 U.S. Census reported Shasta population was 1,771. Shasta State Historic Park located at Shasta is a ghost town and California State Historic Park.',
         choices: [
             {
                 id: 'map',
@@ -277,7 +277,7 @@ export const landmark = [
             top: '36%',
             left: '12%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'The City of Weed, California is nestled at the base of Mount Shasta in the Cascade Mountains and half way between San Francisco and Portland.',
         choices: [
             {
                 id: 'map',
@@ -315,7 +315,7 @@ export const landmark = [
             top: '30%',
             left: '11%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Home to the world-famous Oregon Shakespeare Festival and ranked in the top 10 of “The 100 Best Small Art Towns in America,” Ashland is a cultural hot spot, with award-winning galleries, theaters and restaurants bursting from its seams. Nestled at the base of the Siskiyou and Cascade mountain ranges, Ashland has a reputation for great outdoor recreation in the city limits and beyond. And with the famous Mt. Ashland Ski Area nearby, fun stays in Ashland year-round.',
         choices: [
             {
                 id: 'map',
@@ -353,7 +353,7 @@ export const landmark = [
             top: '25%',
             left: '11%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Home to the world-famous Oregon Shakespeare Festival and ranked in the top 10 of “The 100 Best Small Art Towns in America,” Ashland is a cultural hot spot, with award-winning galleries, theaters and restaurants bursting from its seams. Nestled at the base of the Siskiyou and Cascade mountain ranges, Ashland has a reputation for great outdoor recreation in the city limits and beyond. And with the famous Mt. Ashland Ski Area nearby, fun stays in Ashland year-round.',
         choices: [
             {
                 id: 'map',
@@ -390,7 +390,7 @@ export const landmark = [
         map: {
             top: '20%',
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Salem is the capital city of Oregon. Set amid a park with gardens, the domed Oregon State Capitol contains a collection of art by Oregon artists. The Hallie Ford Museum of Art includes works by Pacific Northwest and Native American artists.',
         choices: [
             {
                 id: 'map',
@@ -428,7 +428,7 @@ export const landmark = [
             top: '15%',
             left: '17%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Woodburn is a city in Marion County, Oregon, United States. Incorporated in 1889, the community had been platted in 1871 after the arrival of the railroad. The city is located in the northern end of the Willamette Valley between Portland and Salem.',
         choices: [
             {
                 id: 'map',
@@ -465,6 +465,6 @@ export const landmark = [
             top: '12%',
             left: '18%'
         },
-        description: 'dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        description: 'Portland, Oregon’s largest city, sits on the Columbia and Willamette rivers, in the shadow of snow-capped Mount Hood. It’s known for its parks, bridges and bicycle paths, as well as for its eco-friendliness and its microbreweries and coffeehouses. Iconic Washington Park encompasses sites from the formal Japanese Garden to Oregon Zoo and its railway. The city hosts thriving art, theater and music scenes',
 
     }];
